8e38fa8fe38b0b955b9871d4eec949202f5fe68c
Hamza Asad <hamza.asad@giki.edu.pk>
2024-10-11 11:23:29 +0500

    feat(sol): add lab 1 solution

---
+++ lab_solutions/lab_1/task_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Step 1: Print user's name
+     cout << "Hello, " << "Name" << "!" << endl;
+    
+    // Step 2: Input an integer
+    cout << "Please enter an integer: ";
+    int number;
+    cin >> number;
+    
+    // Step 3: Print the integer
+    cout << "You entered the number: " << number << endl;
+
+    return 0;
+}
